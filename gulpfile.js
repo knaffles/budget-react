e6cb2552f8a3980caab22379440642c6ae93387f
Michael Girgis <mgirgis@duoconsulting.com>
2018-11-23 10:58:00 -0600

    Rearranging

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function() {
-  return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
+  return gulp.src('public/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
     .pipe(sass())
     .pipe(gulp.dest('public/css'))
 });
 
 gulp.task('watch', function(){
-  gulp.watch('app/scss/**/*.scss', ['sass']); 
+  gulp.watch('public/scss/**/*.scss', ['sass']); 
 });
 
 gulp.task('copyjquery', function() {
