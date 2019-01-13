4271d280f5a8e4a459e3ef791e7934722970f700
knaffles <knaffles@gmail.com>
2019-01-13 12:20:58 -0600

    Rearranging; updating package.json.

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+// import './App.css';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+    }
+  }
+
+  render() {
+    return(
+      <p>Success!</p>
+    );
+  }
+
+}
+
+export default App;
