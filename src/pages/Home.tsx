58029b184cfeb23c1c12e769fe4dcc1146fd6cbf
Michael Girgis <knaffles@gmail.com>
2024-04-05 15:51:36 -0500

    First pass at login form on homepage.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,8 +2,12 @@ import budgetObject from "../../data/budget.json";
 import { addDoc, collection } from "firebase/firestore";
 import { db } from "../services/firebase";
 import Heading from "../components/Heading";
+import { useState } from "react";
 
 const Home = () => {
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
   const handleClick = () => {
     const budget = budgetObject.budget;
     budget.forEach(async (value) => {
@@ -18,21 +22,60 @@ const Home = () => {
     });
   };
 
+  const handleSubmit = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    console.log(username);
+  };
+
   return (
     <>
-      <Heading as="h1">Home</Heading>
-      <p>This is the homepage.</p>
-      <p>
-        <button className="btn" onClick={handleClick}>
-          Import budget data
-        </button>
-      </p>
+      <Heading as="h1">Login</Heading>
 
-      <p>
-        <button className="btn" onClick={handleTransactionsClick}>
-          Import transactions data
-        </button>
-      </p>
+      <form className="max-w-2xl m-auto my-8" onSubmit={handleSubmit}>
+        <div className="flex flex-col gap-4">
+          <label className="form-control w-full">
+            <div className="label pt-0">
+              <span className="label-text">Username</span>
+            </div>
+            <input
+              type="text"
+              className="input input-bordered w-full"
+              onChange={(event) => setUsername(event.target.value)}
+              value={username}
+            />
+          </label>
+          <label className="form-control w-full">
+            <div className="label pt-0">
+              <span className="label-text">Password</span>
+            </div>
+            <input
+              type="password"
+              className="input input-bordered w-full"
+              onChange={(event) => setPassword(event.target.value)}
+              value={password}
+            />
+          </label>
+          <button type="submit" className="btn btn-primary">
+            Log In
+          </button>
+        </div>
+      </form>
+
+      <div role="alert" className="alert">
+        <span>DEV only.</span>
+        <div className="join">
+          <button className="btn btn-neutral join-item" onClick={handleClick}>
+            Import budget data
+          </button>
+
+          <button
+            className="btn btn-neutral join-item"
+            onClick={handleTransactionsClick}
+          >
+            Import transactions data
+          </button>
+        </div>
+      </div>
     </>
   );
 };
